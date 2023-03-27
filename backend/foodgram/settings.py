f222d37e0bc31a4db3062f0d8adc5b870c6d0b91
oupsfed <oups.fedorov@gmail.com>
2023-03-27 18:38:11 +0300

    исправление ошибок, подготовка к ревью

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -74,7 +74,7 @@ DATABASES = {
         'NAME': os.getenv('DB_NAME', default='postgres'),
         'USER': os.getenv('POSTGRES_USER', default='postgres'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', default='3654'),
-        'HOST': os.getenv('DB_HOST', default='localhost'),
+        'HOST': os.getenv('DB_HOST', default='db'),
         'PORT': os.getenv('DB_PORT', default=5432)
     }
 }
